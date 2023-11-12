ba05f9cb5bbb2507f5802fe9a9fc8dd1030cf8bf
Armond-Guze <Armond.Guze@yahoo.com>
2023-11-12 15:35:52 -0500

    added to README, updated javascript file

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # sports-quiz
+
+## PROJECT-4 OF RUTGERS CODING BOOTCMAP COURSE
+
+## PROJECT OVERVIEW
+Created sports quiz program with timer and ability to save your highscores
+
+## HTML 
+We first created an HTML page to apply the basic structure of our program, and classified our selectors to position them in our CSS and Javascript
+
+## CSS
+Our Stylesheet was simple but intuitive. A white box in the center of the screen displays the game while the background matches the button coloring functions
+
+## playQuestion FUNCTION
+We first selected our "Start-Btn" class and made a function to start the game when clicked for the Javascript
--- script.js
+++ script.js
@@ -1,45 +1,118 @@
-const startButton = document.getElementById('start-btn');
+const startButton = document.getElementById("start-btn");
 const questionContainerElement = document.getElementById("question-container");
-var button1 = document.getElementById("answer1")
+var timer = document.getElementById("time");
+var button1 = document.getElementById("answer1");
+var button2 = document.getElementById("answer2");
+var button3 = document.getElementById("answer3");
+var button4 = document.getElementById("answer4");
 
 var currentQuestion = 0;
+var secondsLeft = 30;
 
-var questions = [{
-    question: "What color is the sky?",
-    answers: ["blue", "green", "red", "purple"],
-    correctAnswer: 0
-}, {
-    question: "What color is the grass?",
-    answers: ["blue", "green", "red", "purple"],
-    correctAnswer: 1
-}]
+function setTime() {
+    var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timer.textContent = secondsLeft;
+
+    if (secondsLeft === 0) {
+        clearInterval(timerInterval);
+        sendMessage();
+    }
+
+    }, 1000)
+}
+
+function sendMessage() {
+    timer.textContent = "";
+}
+
+var questions = [
+  {
+    question: " Who is a pro golfer?",
+    answers: ["Tiger Woods", "Tim Mack", "Kobe Bryant", "Pete The Clown"],
+    correctAnswer: 0,
+  },
+  {
+    question: " What is the best football team in New York",
+    answers: ["Rams", "Giants", "Patriots", "Chiefs"],
+    correctAnswer: 1,
+  },
+  {
+    question: " What football player  has the most championships in the NFL?",
+    answers: ["Mike Davis", "Tom Brady", "Michael Jordan", "Daniel Jones"],
+    correctAnswer: 1,
+  },
+  {
+    question: " What city do the Knicks Play for?",
+    answers: ["Idaho", "Nebraska", "Atlanta", "New York"],
+    correctAnswer: 3,
+  },
+];
 
 startButton.addEventListener("click", playQuestion);
 button1.addEventListener("click", checkAnswer1);
+button2.addEventListener("click", checkAnswer2);
+button3.addEventListener("click", checkAnswer3);
+button4.addEventListener("click", checkAnswer4);
 
 function checkAnswer1() {
-    if (questions[currentQuestion].correctAnswer === 0) {
-        alert("you are correct")
-    } else {
-        alert("you are wrong")
-    }
+  if (questions[currentQuestion].correctAnswer === 0) {
+    alert("you are correct");
+  } else {
+    alert("you are wrong");
+  }
 
-    nextQuestion();
+  nextQuestion();
+}
+
+function checkAnswer2() {
+  if (questions[currentQuestion].correctAnswer === 1) {
+    alert("you are correct");
+  } else {
+    alert("you are wrong");
+  }
+
+  nextQuestion();
+}
+
+function checkAnswer3() {
+  if (questions[currentQuestion].correctAnswer === 1) {
+    alert("you are correct");
+  } else {
+    alert("you are wrong");
+  }
+
+  nextQuestion();
+}
+
+function checkAnswer4() {
+  if (questions[currentQuestion].correctAnswer === 3) {
+    alert("you are correct");
+  } else {
+    alert("you are wrong");
+  }
+
+  nextQuestion();
 }
 
 function nextQuestion() {
-    currentQuestion++;
-    playQuestion();
+  currentQuestion++;
+  playQuestion();
 }
-
 
 function playQuestion() {
-console.log("started");
-startButton.classList.add('hide');
-questionContainerElement.classList.remove('hide');
-document.getElementById("question").textContent = questions[currentQuestion].question;
-button1.textContent = questions[currentQuestion].answers[0];
-document.getElementById("answer2").textContent = questions[currentQuestion].answers[1];
-document.getElementById("answer3").textContent = questions[currentQuestion].answers[2];
-document.getElementById("answer4").textContent = questions[currentQuestion].answers[3];
+  console.log("started");
+  startButton.classList.add("hide");
+  questionContainerElement.classList.remove("hide");
+  document.getElementById("question").textContent =
+    questions[currentQuestion].question;
+  button1.textContent = questions[currentQuestion].answers[0];
+  document.getElementById("answer2").textContent =
+    questions[currentQuestion].answers[1];
+  document.getElementById("answer3").textContent =
+    questions[currentQuestion].answers[2];
+  document.getElementById("answer4").textContent =
+    questions[currentQuestion].answers[3];
 }
+
+setTime();
